64789fa815fb1fa65bffc8f9e92ded059f135293
Interest1024 <leo.liubin@gmail.com>
2018-03-18 19:41:36 +1100

    reduced the number of population panels to 3 and added a chart to each panel.

--- app/main/views.py
+++ app/main/views.py
@@ -22,16 +22,13 @@ from contextlib import contextmanager
 import operator
 import re
 from datetime import datetime
-import random
 
-#bokeh:
-from bokeh.models import (HoverTool, FactorRange, Plot, LinearAxis, Grid,
-                          Range1d)
-from bokeh.models.glyphs import VBar
-from bokeh.plotting import figure
-from bokeh.charts import Bar
-from bokeh.embed import components
-from bokeh.models.sources import ColumnDataSource
+#matplotlib
+import numpy
+import matplotlib.pyplot as plt
+from io import BytesIO
+#import base64
+
 
 #schema name in DB
 SCHEMA_DATA = "census_2016_data"
@@ -334,6 +331,17 @@ def get_population_data(InputSSC, InputSuburb):
                 response2_item['numoftable'] = 1
                 response2_item['table'] = [table1]
                 response2_item['numofchart'] = 1
+                #make chart
+                x_values = [1,2,3,4,5,6,7,8,9,10,11]
+                x_labels = ["0-4","5-14","15-19","20-24","25-34","35-44","45-54","55-64","65-74","75-84",">85"]
+                y_values = [int(value_dict["g6"]),int(value_dict["g9"]),int(value_dict["g12"]),int(value_dict["g15"]), 
+                            int(value_dict["g18"]),int(value_dict["g21"]),int(value_dict["g24"]),int(value_dict["g27"]), \
+                            int(value_dict["g30"]),int(value_dict["g33"]),int(value_dict["g36"])]
+                filepath = 'app/main/static'
+                filename = 'chart_g3_01.png'
+                chart1 = create_population_chart_g3(x_values,x_labels,y_values,filepath,filename)
+                response2_item['chart'] = [filename]
+                
             # for total males    
             elif response_item["id"].lower() == "g2": 
                 table1 = []
@@ -343,6 +351,17 @@ def get_population_data(InputSSC, InputSuburb):
                 response2_item['numoftable'] = 1
                 response2_item['table'] = [table1]
                 response2_item['numofchart'] = 1
+                #make chart
+                x_values = [1,2,3,4,5,6,7,8,9,10,11]
+                x_labels = ["0-4","5-14","15-19","20-24","25-34","35-44","45-54","55-64","65-74","75-84",">85"]
+                y_values = [int(value_dict["g5"]),int(value_dict["g8"]),int(value_dict["g11"]),int(value_dict["g14"]), 
+                            int(value_dict["g17"]),int(value_dict["g20"]),int(value_dict["g23"]),int(value_dict["g26"]), \
+                            int(value_dict["g29"]),int(value_dict["g32"]),int(value_dict["g35"])]
+                filepath = 'app/main/static'
+                filename = 'chart_g3_02.png'
+                chart1 = create_population_chart_g3(x_values,x_labels,y_values,filepath,filename)
+                response2_item['chart'] = [filename]
+
             # for total females    
             elif response_item["id"].lower() == "g1": 
                 table1 = []
@@ -352,6 +371,16 @@ def get_population_data(InputSSC, InputSuburb):
                 response2_item['numoftable'] = 1
                 response2_item['table'] = [table1]
                 response2_item['numofchart'] = 1
+                #make chart
+                x_values = [1,2,3,4,5,6,7,8,9,10,11]
+                x_labels = ["0-4","5-14","15-19","20-24","25-34","35-44","45-54","55-64","65-74","75-84",">85"]
+                y_values = [int(value_dict["g4"]),int(value_dict["g7"]),int(value_dict["g10"]),int(value_dict["g13"]), 
+                            int(value_dict["g16"]),int(value_dict["g19"]),int(value_dict["g22"]),int(value_dict["g25"]), \
+                            int(value_dict["g28"]),int(value_dict["g31"]),int(value_dict["g34"])]
+                filepath = 'app/main/static'
+                filename = 'chart_g3_03.png'
+                chart1 = create_population_chart_g3(x_values,x_labels,y_values,filepath,filename)
+                response2_item['chart'] = [filename]
 
             response2_list.append(response2_item)
 
@@ -644,75 +673,26 @@ def get_data():
 #########################################
 
 
-@main.route("/chart/<int:bars_count>/")
-def chart(bars_count):
-    """Try to show chart"""
-    if bars_count <= 0:
-        bars_count = 1
 
-    data = {"days": [], "bugs": [], "costs": []}
-    for i in range(1, bars_count + 1):
-        data['days'].append(i)
-        data['bugs'].append(random.randint(1,100))
-        data['costs'].append(random.uniform(1.00, 1000.00))
 
-    hover = create_hover_tool()
-    plot = create_bar_chart(data, "Bugs found per day", "days",
-                            "bugs", hover)
-    script, div = components(plot)
+def create_population_chart_g3(x_values,x_labels,y_values,filepath,filename):
+    ### Generating X,Y coordinaltes to be used in plot
+    ### Generating The Plot
+    plt.bar(x_values,y_values)
+    plt.xticks(x_values, x_labels)
+    ### Saving plot to disk in png format
+    filepathname = filepath+'/'+filename
+    plt.savefig(filepathname)
 
-    return render_template("chart.html", bars_count=bars_count,
-                           the_div=div, the_script=script)
-
-def create_hover_tool():
-    """Generates the HTML for the Bokeh's hover data tool on our graph."""
-    hover_html = """
-      <div>
-        <span class="hover-tooltip">$x</span>
-      </div>
-      <div>
-        <span class="hover-tooltip">@bugs bugs</span>
-      </div>
-      <div>
-        <span class="hover-tooltip">$@costs{0.00}</span>
-      </div>
-    """
-    return HoverTool(tooltips=hover_html)
-
-def create_bar_chart(data, title, x_name, y_name, hover_tool=None,
-                     width=1200, height=300):
-    """Creates a bar chart plot with the exact styling for the centcom
-       dashboard. Pass in data as a dictionary, desired plot title,
-       name of x axis, y axis and the hover tool HTML.
-    """
-    source = ColumnDataSource(data)
-    xdr = FactorRange(factors=data[x_name])
-    ydr = Range1d(start=0,end=max(data[y_name])*1.5)
-
-    tools = []
-    if hover_tool:
-        tools = [hover_tool,]
-
-    plot = figure(title=title, x_range=xdr, y_range=ydr, plot_width=width,
-                  plot_height=height, h_symmetry=False, v_symmetry=False,
-                  min_border=0, toolbar_location="above", tools=tools,
-                  responsive=True, outline_line_color="#666666")
-
-    glyph = VBar(x=x_name, top=y_name, bottom=0, width=.8,
-                 fill_color="#e12127")
-    plot.add_glyph(source, glyph)
-
-    xaxis = LinearAxis()
-    yaxis = LinearAxis()
-
-    plot.add_layout(Grid(dimension=0, ticker=xaxis.ticker))
-    plot.add_layout(Grid(dimension=1, ticker=yaxis.ticker))
-    plot.toolbar.logo = None
-    plot.min_border_top = 0
-    plot.xgrid.grid_line_color = None
-    plot.ygrid.grid_line_color = "#999999"
-    plot.yaxis.axis_label = "Bugs found"
-    plot.ygrid.grid_line_alpha = 0.1
-    plot.xaxis.axis_label = "Days after app deployment"
-    plot.xaxis.major_label_orientation = 1
-    return plot    
+    ### Rendering Plot in Html
+    figfile = BytesIO()
+    plt.savefig(figfile, format='png')
+    # to clear old data
+    plt.clf()
+    plt.cla()
+    plt.close()
+    
+    # figfile.seek(0)
+    # figdata_png = base64.b64encode(figfile.getvalue())
+    # result = figdata_png
+    # return result
